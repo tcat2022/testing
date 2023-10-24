4a89009d58f283a6b2e83fb02d79f9b6045f3931
tommy <tcatty7@gmail.com>
2023-10-23 21:09:12 -0400

    checking how it looks

--- stopwatch.js
+++ stopwatch.js
@@ -1,3 +1,5 @@
 let width = document.getElementById('watch-container').clientWidth;
+let height = document.getElementById('watch-container').clientHeight;
 let watchContainer = document.getElementById('watch-container');
-watchContainer.style.width = width + 20 + 'px'
+watchContainer.style.width = width + 200 + 'px'
+watchContainer.style.height = height + 150 + 'px'
